d187a5893c424a0da7df6fe0fbab2552e13648d6
Chris Bocardo <cbocardo@vpayusa.com>
2019-10-12 08:36:12 -0500

    working through braveclojure.com

---
+++ brave-clojure/src/brave_clojure/core.clj
@@ -0,0 +1,32 @@
+(ns brave-clojure.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
+
+(defn map-set-loop
+  "Variation of 'map' that returns a set. Uses 'loop' for evaluation."
+  [f collection]
+  (loop [c collection
+         mapped-set #{}]
+    (if (empty? c)
+      mapped-set
+      (recur (rest c) (into mapped-set [(f (first c))])))))
+
+(defn map-set
+  "Variation of 'map' that returns a set. Uses 'reduce' for evaluation."
+  [func collection]
+  (fn (reduce [coll item]
+            (into coll (set [(func item)])))
+          #{}
+          collection))
+
+(defn map-reduce
+  "Implementation of 'map' using reduce"
+  [func collection]
+  (reduce (fn [coll item]
+            (into coll (set [(func item)])))
+          '()
+          collection))
